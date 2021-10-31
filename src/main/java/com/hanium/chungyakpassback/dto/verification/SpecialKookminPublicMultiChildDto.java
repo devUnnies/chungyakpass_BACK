d54adb0d984bdef4ec7eecd0d5689df647ab1b03
jieunee2 <980lje@naver.com>
2021-10-31 11:31:09 +0900

    not null 추가

--- src/main/java/com/hanium/chungyakpassback/dto/verification/SpecialKookminPublicMultiChildDto.java
+++ src/main/java/com/hanium/chungyakpassback/dto/verification/SpecialKookminPublicMultiChildDto.java
@@ -2,12 +2,18 @@ package com.hanium.chungyakpassback.dto.verification;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SpecialKookminPublicMultiChildDto {
+
+    @NotNull
     public Integer notificationNumber;
+
+    @NotNull
     public String housingType;
 }
